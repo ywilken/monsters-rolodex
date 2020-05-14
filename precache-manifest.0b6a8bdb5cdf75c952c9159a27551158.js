726f1dc7f0356d946f7765e43166a0d91e50689d
Yannick Wilken <61846545+ywilken@users.noreply.github.com>
2020-05-14 11:14:15 -0500

    Updates

---
+++ precache-manifest.0b6a8bdb5cdf75c952c9159a27551158.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf0c072c4f9d82ef302dcf473847401",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dd1a70151f5620b028ad",
+    "url": "/monsters-rolodex/static/css/main.e9aa77dc.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd1a70151f5620b028ad",
+    "url": "/monsters-rolodex/static/js/main.41503692.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
