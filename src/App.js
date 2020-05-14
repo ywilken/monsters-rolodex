f7642ad6739717d034c20bcdc3e8c4c2e223b0eb
Yannick Wilken <61846545+ywilken@users.noreply.github.com>
2020-05-14 10:55:32 -0500

    adding files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component.jsx';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ""
+    }
+  }
+  
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    // .then(newResponse => console.log(newResponse))
+    .then(users => this.setState( {monsters: users} ))
+  }
+
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value});
+  }
+
+  render() {
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster => {
+      const filteredByName = monster.name.toLowerCase().includes(searchField.toLowerCase());
+      const filteredByEmail =  monster.email.toLowerCase().includes(searchField.toLowerCase());
+      return filteredByName + filteredByEmail;
+    })
+    return (
+      <div className="App">
+        <SearchBox 
+          placeholder = "search monster"
+          handleChange = {this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}/>
+    </div>
+    );
+  }
+
+  // componentDidUpdate() {
+  //   console.log(this.state)
+  // }
+}
+
+
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       string: 'Hello Nick...'
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           {this.state.string}
+//         </p>
+//         <h2>
+//           What a nice idea!
+//         </h2>
+//         <button onClick={() => this.setState({string: 'Hello Monica...'})}>Change Text</button>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//     );
+//   }
+// }
+
+
+
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +94,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2>
+          What a nice idea!
+        </h2>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +108,6 @@ function App() {
       </header>
     </div>
   );
-}
+} */
 
 export default App;
